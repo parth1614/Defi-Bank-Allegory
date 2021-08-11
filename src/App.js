6b87c0da2325583710e2959e63064710ed70ac3f
Parth Arora <60436959+parth1614@users.noreply.github.com>
2021-08-11 12:20:58 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="app_header">
+        <Header />
+      </div>
+      <div className="app_body">
+
+          <div className="app_container">
+              
+          </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
